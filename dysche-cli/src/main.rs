f33abde56752f1dad4a9b80c656de08fd85989bf
Liang Li (Euler) <liliang889@huawei.com>
2021-06-09 13:54:43 +0800

    dysche-cli: revised parameter parser to be yaml based
    Signed-off-by: Liang Li (Euler) <liliang889@huawei.com>

--- dysche-cli/src/main.rs
+++ dysche-cli/src/main.rs
@@ -1,17 +1,19 @@
 extern crate clap;
-use clap::clap_app;
 
 fn main() {
-    let _matches = clap_app!(dyschecli =>
-		(version: "0.1.0")
-		(author: "Guangxing Deng. <dengguangxing@huawei.com>")
-		(about: "Cli tools for dysche partition management")
-		(@arg help: -h ... "Show dysche-cli help infos")
-		(@subcommand list =>
-			(about: "Show availible partitions")
-			(version: "0.1.0")
-			(author: "Guangxing Deng. <dengguangxing@huawei.com>")
-			(@arg verbose: -v --verbose "Print partiton information verbosely")
-		)
-	).get_matches();
+    let yml = clap::load_yaml!("param.yml");
+    let matches = clap::App::from_yaml(yml).get_matches();
+
+    if let Some(_sc) = matches.subcommand_matches("list") {
+        if _sc.is_present("verbose") {
+            println!("List partition details: ");
+        } else {
+            println!("List partitions:");
+        }
+    } else if let Some(_sc) = matches.subcommand_matches("create") {
+        println!("create app partition:");
+    } else {
+        let _ = clap::App::from_yaml(yml).print_long_help();
+    }
+
 }
