0dbbffafe8af1899167f8f7e0b9ed7843bf938ef
Liang Li <liliang6@me.com>
2021-12-29 14:08:19 +0800

    tools: initramfs.sh: update setup for console
    Signed-off-by: Liang Li <liliang6@me.com>

--- tools/initramfs.sh
+++ tools/initramfs.sh
@@ -1,2 +1,6 @@
 yum install bash --installroot /root/root-t
+
+sudo mknod /root/root-t/dev/console c 5 1
+sudo chown root:tty /root/root-t/dev/console
+
 find . | cpio --quiet -H newc -o | gzip -9 -n > ../initramfs.img
