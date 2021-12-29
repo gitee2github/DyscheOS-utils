e0ab4fa7ab9c3b95d4814a5edee03f11c7b7bc4a
Liang Li <liliang6@me.com>
2021-12-30 00:09:33 +0800

    tools: commit misc changes for tools
    Signed-off-by: Liang Li <liliang6@me.com>

--- tools/run_qemu_master.sh
+++ tools/run_qemu_master.sh
@@ -12,11 +12,9 @@ qemu-system-riscv64 -nographic \
 		    -device virtio-blk-device,drive=hd0 \
 		    -device virtio-net-device,netdev=usernet \
 		    -netdev user,id=usernet,hostfwd=tcp::12055-:22 \
-		    -append 'root=/dev/vda1 irqpoll mem=1G rootwait rw console=ttyS0 systemd.default_timeout_start_sec=60 selinux=0 highres=off earlycon'
+		    -append 'rdinit=/bin/sh root=/dev/vda1 irqpoll mem=1G rootwait rw console=ttyS0 systemd.default_timeout_start_sec=60 selinux=0 highres=off earlycon' \
+		    -serial tcp::12121,server,nowait \
+                    -serial tcp::12122,server,nowait \
+                    -serial tcp::12123,server,nowait \
+		    -serial tcp::12124,server,nowait
 
-# dysche_reserve=6G@0x100000000
-#                     -serial tcp::12121,server,nowait \
-#                     -serial tcp::12122,server,nowait
-#
-# nc localhost 12121
-# nc localhost 12122
