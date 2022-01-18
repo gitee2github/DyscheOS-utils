627ee159d159843cdeeeaf37d9a819489b70265e
Liang Li <liliang6@me.com>
2022-01-18 14:45:58 +0800

    tools/initramfs.sh: add a few more records around initramfs stuff
    Signed-off-by: Liang Li <liliang6@me.com>

--- tools/initramfs.sh
+++ tools/initramfs.sh
@@ -1,6 +1,17 @@
-yum install bash --installroot /root/root-t
+#!/bin/bash
 
-sudo mknod /root/root-t/dev/console c 5 1
-sudo chown root:tty /root/root-t/dev/console
+ROOTFS_ROOT="/root/root-t/"
+yum install bash --installroot $ROOTFS_ROOT
 
-find . | cpio --quiet -H newc -o | gzip -9 -n > ../initramfs.img
+sudo mknod $ROOTFS_ROOT/dev/console c 5 1
+sudo chown root:tty $ROOTFS_ROOT/dev/console
+
+cd $ROOTFS_ROOT
+sudo rm -rf usr/lib/locale/locale-archive*
+sudo rm -rf var/cache/dnf/
+sudo rm -rf usr/share/locale/
+
+sudo find . | sudo cpio --quiet -H newc -o  > ../initramfs.cpio
+cd ..
+
+
